2ec7bfb2ab54ec5b51f21fcd1a261ebcb6f13da7
Komal Sinha <sinhakomal589@gmail.com>
2026-06-24 14:48:42 +0530

    max-subarray  array questions

---
+++ java_dsa/Array/practice/Index_manipulation/left_rotate_by_Dplace.java
@@ -0,0 +1,29 @@
+package practice.Index_manipulation;
+import java.util.*;
+
+public class left_rotate_by_Dplace {
+    public static void reverse_Array(int arr[],int start,int end){
+        while(start<end){
+           int temp=arr[start];
+        arr[start]=arr[end];
+        arr[end]=temp;
+        start++;
+        end--;
+        }
+    }  
+    public static void left_rotate(int arr[],int d){
+        int n= arr.length;
+         d = d % n;
+        reverse_Array(arr,0,d-1);
+        reverse_Array(arr,d,n-1);
+        reverse_Array(arr,0, n-1);
+    
+    }
+      public static void main(String[] args) {
+        int arr[]={1,2,3,4,5,6,7};
+        int d=3;
+     left_rotate(arr, d);
+    System.out.println(Arrays.toString(arr));
+   
+    }
+}
