8ee0153e872531c861b2f29cd22043489e5f5227
Komal Sinha <sinhakomal589@gmail.com>
2026-06-25 20:27:18 +0530

    right rotation by d places  array questions

---
+++ java_dsa/Array/practice/Index_manipulation/right_rotation_D.java
@@ -0,0 +1,30 @@
+package practice.Index_manipulation;
+
+import java.util.Arrays;
+
+public class right_rotation_D {
+    public static void reverse_Array(int arr[],int start,int end){
+        while(start<end){
+           int temp=arr[start];
+        arr[start]=arr[end];
+        arr[end]=temp;
+        start++;
+        end--;
+        }
+    }
+
+    public static void right_rotation_D(int arr[],int d){
+        int n= arr.length;
+        d = d % n;
+        reverse_Array(arr,0, n-d-1);
+        reverse_Array(arr, n-d, n-1);
+        reverse_Array(arr, 0, n-1);
+
+    }
+    public static void main(String[] args) {
+        int arr []={1,2,3,4,5,6,7};
+        int d=3;
+        right_rotation_D(arr, d);
+        System.out.println(Arrays.toString(arr));
+    }
+}
