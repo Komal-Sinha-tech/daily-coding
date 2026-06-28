088c6ea8e19084f7aefc360364c15ef60bfa5110
Komal Sinha <sinhakomal589@gmail.com>
2026-06-28 12:39:04 +0530

    sorting algo - Selection sort  questions

--- java_dsa/Sorting/Selection_Sort/des_Selection.java
+++ java_dsa/Sorting/Selection_Sort/des_Selection.java
@@ -1,21 +1,19 @@
 
-
-public class asc_Selection {
-    public static void asc_Selection(int arr[]){
+public class des_Selection {
+    public static void des_Selection(int arr[]){
         for(int i=0;i<arr.length-1;i++){
-            int minPos = i;
+            int maxPos =i;
             for(int j=i+1;j<arr.length;j++){
-                if(arr[j]<arr[minPos]){
-                    minPos=j;
+                if(arr[j]>arr[maxPos]){
+                    maxPos=j;
                 }
             }
             //swap
-            int temp=arr[minPos];
-            arr[minPos]=arr[i];
+            int temp=arr[maxPos];
+            arr[maxPos]=arr[i];
             arr[i]=temp;
         }
     }
-
     public static void print_Array(int arr[]){
         for(int num : arr){
             System.out.print(num+" ");
@@ -23,7 +21,7 @@ public class asc_Selection {
     }
     public static void main(String[] args) {
         int arr[]={2,5,3,7,9,1,6,5,8};
-           asc_Selection(arr);
-           print_Array(arr);
+        des_Selection(arr);
+        print_Array(arr);
     }
 }
