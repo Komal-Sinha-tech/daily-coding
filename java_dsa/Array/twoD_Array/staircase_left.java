c868ba624dfa0332d44076520f7d7c9196785faf
Komal Sinha <sinhakomal589@gmail.com>
2026-07-06 15:03:58 +0530

    2D Array  questions

--- java_dsa/Array/twoD_Array/staircase_left.java
+++ java_dsa/Array/twoD_Array/staircase_left.java
@@ -1,23 +1,23 @@
 package twoD_Array;
 
-public class staircaseSearch {
-    public static boolean staircaseSearch_rightMost(int matrix[][],int key){
-        int row=0;
-        int col=matrix[0].length-1;
-        while(row<matrix.length && col>=0){
+public class staircase_left {
+    public static boolean staircaseSearch_leftMost(int matrix[][],int key){
+        int row=matrix.length-1;
+        int col=0;
+        while(row >= 0 && col < matrix[0].length){
             if(matrix[row][col]== key){
                 System.out.println("found key at ("+row+","+col+") matrix ");
                 return true;
             }else if(key<matrix[row][col]){
-                col--;
+                row--;
             }else{
-                row++;
+                col++;
             }
         }
         System.out.println("key not found");
         return false;
     }
-       public static void main(String[] args) {
+    public static void main(String[] args) {
         int matrix[][]={
             {1,2,3,4},
             {5,6,7,8},
@@ -25,6 +25,7 @@ public class staircaseSearch {
             {13,14,15,16},
         };
         int key=15;
-        staircaseSearch_rightMost(matrix, key);
+        staircaseSearch_leftMost(matrix, key);
+        
     }
 }
