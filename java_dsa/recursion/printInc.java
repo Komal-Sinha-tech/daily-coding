7a561edbd16311003d8f10c46f213701b8af452f
Komal Sinha <sinhakomal589@gmail.com>
2026-08-15 14:26:53 +0530

    recursion  questions

---
+++ java_dsa/recursion/printInc.java
@@ -0,0 +1,16 @@
+package recursion;
+
+public class printInc {
+    public static void printInc(int n){
+        if(n==1){
+            System.out.print(n+" ");
+            return;
+        }
+        printInc(n-1);
+        System.out.print(n+" ");
+    }
+    public static void main(String[] args){
+    int n=5;
+    printInc(n);
+    }
+}
